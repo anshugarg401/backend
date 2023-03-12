f9dc121ade73d3a09a55968601d6be616bfcf042
anshugarg401 <92512766+anshugarg401@users.noreply.github.com>
2023-03-12 14:24:05 +0530

    gr

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-const mongoURL = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+const mongoURL = "mongodb+srv://anshugarg401:Sy3T2nStW4XqY1p7@cluster0.imi0h76.mongodb.net/test"
 const connecttomongo = ()=>{mongoose.connect(mongoURL,()=>{console.log('success fully connected to mongodb');})} 
 module.exports = connecttomongo;
